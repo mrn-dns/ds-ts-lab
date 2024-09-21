1590324592c61959ff6e2f0693149b64a93d2447
Denis <marincasdenis@yahoo.com>
2024-09-21 20:03:11 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -91,3 +91,15 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, newInterest: string) {
+  if (friend.interests == undefined) {
+    friend.interests = [];
+  }
+
+  friend.interests.push(newInterest);
+
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], "Politics"));
